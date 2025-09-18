fff1e5efc5caea73f4ffb294636b4d3b9506d5af
Evelin I. Mojica Quintana <20243ds170@utez.edu.mx>
2025-09-18 13:58:44 -0600

    Fibonacci

---
+++ Fibonacci/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    public static void main(String[] args) throws Exception {
+        int numero = 5;
+
+        for(int i =0; i<numero; i++){
+            System.out.println(fibonacci(i) + " ");
+        }
+    }
+}
