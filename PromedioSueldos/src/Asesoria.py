55862680dcd2e900ace8249c3110eca773c85c16
Evelinmq <20243ds170@utez.edu.mx>
2025-10-06 09:05:47 -0600

    Trabajo

---
+++ PromedioSueldos/src/Asesoria.py
@@ -0,0 +1,60 @@
+
+"""
+Se requiere consumir el archivo de texto Alumnos_Editado.txt
+Con la información de los alumnos se requere hacer lo sieguinete:
+
+1.- Ordenar todas las materias con el siguiente formato
+(MAYUS) (A-Z) INFORMÁTICA
+
+2.- Devolver en consola los 3 promedios más altos en un Diccionario:
+{
+    'Nombre': 'Nombre Alumno que agregaron de tarea',
+    'Primedio': Primerio en tipo de dato FLOAT
+    'Rank': '1°' #Segun sea el Caso 1°, 2° o 3°
+}
+"""
+"""
+lista_temp = ['5','4','3','2','1']
+lista_dispositivos = {"R1": "127.0.0.1/31", "R2": "0.0.0.1/31", "R3": "1.0.0.0/8"}
+routers = str(lista_dispositivos)
+
+print("Dict: " + lista_dispositivos)
+"""
+
+ipAddress = {'R1': '10.1.1.1', 'R2': '10.2.2.1', 'R3': '10.3.3.1'}
+ipAddress ["R4"] = ["10.3.3.1"]
+x = "1"
+
+if(x == 1):
+    print("True")
+    0 == 0
+
+if(x === 1):
+    print("True")
+
+"""
+devices = ["R1", "R2", "R3", "S1", "S2"]
+"""
+"""
+for device in devices:
+    print (str(device))
+"""
+"""   
+newDivicesList = []
+
+for id,value in ipAddress.items():
+     newDivicesList.append(id)
+     newDivicesList.append(value)
+     if(id == "R4"):
+        0 == 0
+        newDivicesList.append(id)
+        newDivicesList.append(str(value[0]))
+
+        print(str(newDiviceList))
+"""
+"""
+if (lista_dispositivos.get("R1") > lista_temp[0]):
+    print("Esto es bloque True")
+else:
+    print("Esto es bloque False")
+"""
