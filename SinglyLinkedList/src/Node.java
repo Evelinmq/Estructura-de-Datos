b38bc0010fe32041a80dace77fdc492ded71e5ad
Evelimq <20243ds170@utez.edu.mx>
2025-10-16 08:41:17 -0600

    SinglyLinkedList

---
+++ SinglyLinkedList/src/Node.java
@@ -0,0 +1,13 @@
+public class Node {
+
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        //this.next = null; <-----Se puede omitir ya que por default los valores se inicializan en cero
+    }
+
+    
+
+}
---
+++ SinglyLinkedList/src/SinglyLinkedList.java
@@ -0,0 +1,72 @@
+public class SinglyLinkedList {
+
+    Node head;
+
+    public void add(int data){
+        Node newNode =  new Node(data); //Creación de un nuevo ndo
+
+        if(head == null){ //Verificación de la lista vacía
+            head = newNode;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) { //Recorrido de los nodos, hasta encontrar el último
+            current = current.next;
+        }
+
+        current.next = newNode; //Inserta nuevo nodo al fina de la lista
+    }
+
+    public boolean contains(int data){
+        /*if(head == null){
+            System.out.println("Tu lista está vacía");
+            return false;
+        }
+            Notifica al usuario
+            */
+
+        Node current = head;
+        while (current != null) {
+            if(current.data == data){
+                return true; //Evitamos que se siga ejecutando
+            }
+
+            current = current.next;
+        }
+        
+        return false;
+    }
+
+    //Remover por valor
+    public void remove(int data){
+        if(head == null){ //Pregunta si la lista está vacía
+            return;
+        }
+
+        if(head.data == data){ //Valida que el dato esté en el head
+            head = head.next; //Elimina el primer nodo
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null && current.next.data != data) { //Recorrer hasta encontrar el valor
+            //Estándar de recorrido && pregunta si el valor es el que se busca
+
+            current = current.next;
+        }
+
+        current.next = current.next.next; //Asignamos el nodo corriente al siguiente
+
+    }
+
+    public void printList(){
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+}
