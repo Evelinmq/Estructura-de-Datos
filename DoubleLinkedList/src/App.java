15b019c85c861174ea0f27b0b0de8fa6a143005e
Evelinmq <20243ds170@utez.edu.mx>
2025-10-17 17:56:11 -0600

    Ejercicio

--- DoubleLinkedList/src/App.java
+++ DoubleLinkedList/src/App.java
@@ -2,17 +2,22 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         DoubleLinkedList list = new DoubleLinkedList();
+        
+        System.out.println("-----------Imprimir lista sin datos---------");
         list.printForward();
+
         list.add(1);
         list.add(2);
         list.add(5);
         list.add(6);
+
+        System.out.println("-----------Imprimir forward---------");
         list.printForward();
-        System.out.println("-----------Imprimir backward---------");
-        list.printBackward();
-        
         System.out.println("-----------Remover de la lista---------");
         list.remove(5);
+        System.out.println("Forward");
+        list.printForward();
+        System.out.println("Backward");
         list.printBackward();
     }
 }
--- DoubleLinkedList/src/DoubleLinkedList.java
+++ DoubleLinkedList/src/DoubleLinkedList.java
@@ -1,46 +1,70 @@
 public class DoubleLinkedList {
 
     Node head;
+    Node tail;
 
     public void add(int data){
         Node newNode = new Node(data);
 
         if (head == null) {
             head = newNode;
+            tail = newNode;
             return;
         }
 
-        Node current = head;
+        //Node current = head;
 
-        while (current.next != null) {
-            current = current.next;
-        }
+        //while (current.next != null) {
+          //  current = current.next;
+        //}
 
-        current.next = newNode;
-        newNode.prev = current;
-
+        tail.next = newNode;
+        //current.next = newNode;
+        //newNode.prev = tail;
+        newNode.prev = tail;
+        tail = newNode;
         
     }
 
     public void printForward(){
         Node current = head;
-        while (current != null) { //Pregunta si tiene referencia
+        /*while (current != null) { //Pregunta si tiene referencia
             System.out.println(current.data); 
             current = current.next;
         }
-        System.out.println(current);
+        System.out.println(current);*/
+        if (current == null) {
+            System.out.println("La lista está vacía");
+            return;
+        }
+        
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+        //System.out.println(current);
+        System.out.println();
     }
 
     public void printBackward(){
-        Node current = head;
-        while (current.next != null) {
-            current = current.next;
+        //Node current = head;
+        Node current = tail;
+
+        if (current == null) {
+            System.out.println("La lista está vacía");
+            return;
         }
+        
+        /*while (current.next != null) {
+            current = current.next;
+        }*/
+
         while (current != null) { //Reversa
             System.out.println(current.data);
             current = current.prev;
         }
-        System.out.println(current);
+        //System.out.println(current);
+        System.out.println();
     }
 
     public boolean contains(int data){
@@ -67,23 +91,55 @@ public class DoubleLinkedList {
             System.out.println("Tu lista está vacía");
             return;
         }
+
+        Node current = head;
         
 
-        if(head.data == data){ //Valida que el dato esté en el head
+        /*if(head.data == data){ //Valida que el dato esté en el head
             head = head.next; //Elimina el primer nodo
             return;
         }else{
             System.out.println("valor no encontrado");
+        }*/
+
+        while (current != null && current.data != data) {
+            current = current.next;
         }
 
-        Node current = head;
-        while (current.next != null && current.next.data != data) { //Recorrer hasta encontrar el valor
+        if (current == null) {
+            System.out.println("Valor no encontrado");
+            return;
+        }
+
+        //Caso A: head
+        if (current == head) {
+            head = head.next;
+            if (head != null) {
+                head.prev = null;
+            }else{
+                tail = null;
+            }
+            return;
+        }
+
+        //Caso B: medio
+        current.prev.next = current.next;
+        current.next.prev = current.prev;
+
+        //Caso C: tail
+        if (current == tail) {
+            tail = tail.prev;
+            tail.next = null;
+            return;
+        }
+
+        /*while (current.next != null && current.next.data != data) { //Recorrer hasta encontrar el valor
             //Estándar de recorrido && pregunta si el valor es el que se busca
 
             current = current.next;
         }
 
         current.next = current.next.next; //Asignamos el nodo corriente al siguiente
-
+        */
     }
 }
