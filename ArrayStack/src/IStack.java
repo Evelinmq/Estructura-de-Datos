7a24c5d08c5a9e16be6d0776f35c395fa802e5df
Evelinmq <20243ds170@utez.edu.mx>
2025-10-23 08:57:59 -0600

    Array Stack

---
+++ ArrayStack/src/IStack.java
@@ -0,0 +1,12 @@
+public interface IStack<T> {
+
+    //Decaración de sus métodos, pero no de sus bloques
+    void push(T data);
+    T pop();
+    T peek();
+    void clear();
+    int size();
+    boolean isEmpty();
+    void print();
+
+}
