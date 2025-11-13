e98c3a6689bf18289096dc9e1629c9ee94df572b
Evelinmq <20243ds170@utez.edu.mx>
2025-11-13 07:23:40 -0600

    BubbleSort

---
+++ BubbleSort/src/import java.util.java
@@ -0,0 +1,12 @@
+ import java.util.scanner;
+ public class Integradora {
+    public static void main(String [] args) {
+        Scanner scanner = new Scanner(System.in);
+        double cal, prom;
+        String nom_alum, nom_mat;
+        int num_uni, num_alum;
+
+        
+
+    }
+ }
