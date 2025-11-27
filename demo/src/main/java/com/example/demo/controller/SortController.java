df32ee9b43246c64375e7825c980ff7299dbcbda
Evelinmq <20243ds170@utez.edu.mx>
2025-11-27 08:38:33 -0600

    Api Service

--- demo/src/main/java/com/example/demo/controller/SortController.java
+++ demo/src/main/java/com/example/demo/controller/SortController.java
@@ -4,11 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.CustomRequest;
+import com.example.demo.dto.ErrorResponse;
 import com.example.demo.service.SortService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -19,17 +24,54 @@ public class SortController {
     SortService service;
 
     @PostMapping("/insertion") //endpoint
-    public int[] postMethodName(@RequestBody CustomRequest request) {
+    public ResponseEntity<?> postMethodName(@RequestBody CustomRequest request) {
         
-        String requestData = request.getData();
-        String[] stringData = requestData.split(",");
-        int[] arrNum = new int[stringData.length];
-        for (int i = 0; i < stringData.length; i++) {
-            arrNum[i] = Integer.parseInt(stringData[i]);
-        }
+        //try {
+            if (request.getData() == null || request.getData().isBlank()) {
+                ErrorResponse error = new ErrorResponse();
+                error.setError("Error en la entrada de datos");
+                error.setDetail("Necesitamos que llenes data con datos numérios");
+                return ResponseEntity.badRequest().body(error);
+            }
 
-        return service.sort(arrNum);
+            if (!request.getData().contains(",")) {
+                ErrorResponse error = new ErrorResponse();
+                error.setError("Error de datos de entrada");
+                error.setDetail("Necesito las comas...");
+                return ResponseEntity.badRequest().body(error);
+            }
+
+            String requestData = request.getData();
+            String[] stringData = requestData.split(",");
+            int[] arrNum = new int[stringData.length];
+            for (int i = 0; i < stringData.length; i++) {
+                try {
+                    arrNum[i] = Integer.parseInt(stringData[i]);
+                } catch (Exception e) {
+                    ErrorResponse error = new ErrorResponse();
+                    error.setError("hubo un error de datos");
+                    error.setDetail("El dato: " + arrNum[i] + " es un dato incorrecto");
+                    return ResponseEntity.badRequest().body(error);
+                }
+                
+            }
+
+            return ResponseEntity.ok(service.sort(arrNum));
+        /*} catch (Exception e) {
+            ErrorResponse error = new ErrorResponse();
+            error.setError("Error general");
+            error.setDetail("Error en la petición");
+            return ResponseEntity.badRequest().body(error);
+        }*/
+
+        
     }
+
+    @GetMapping("path")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
     
 
 }
